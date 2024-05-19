428eb901aa585761a697a0fbb405fd91206d9953
Olaibi Samad Adekunle <danielolaibi@gmail.com>
2024-05-19 14:04:36 +0100

    First commit Application running

---
+++ src/test/java/com/in3rovert_so/securedoc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in3rovert_so.securedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
